2dcdabb1e534d99b0bc3906f444f9ce43e519bbf
Steven <steven.a.pinkerton@gmail.com.com>
2023-08-19 19:38:57 +0100

    Textures errors resolved

--- src/AssetLoader/Textures.hs
+++ src/AssetLoader/Textures.hs
@@ -1,18 +1,17 @@
 module AssetLoader.Textures where
 
 import AssetLoader.Datatypes ( Texture(..) )
-import Text.XML.Cursor ( attribute, element, ($//), (&|), Cursor )
+import Text.XML.Cursor ( attribute, element, ($//), (&|), Cursor, (&//) )
 
 parseTextures :: Cursor -> [Texture]
 parseTextures c =
-  c
-    $// element "{DAE_NAMESPACE}library_textures"
-    $// element "{DAE_NAMESPACE}texture"
+  c $// element "{DAE_NAMESPACE}library_textures"
+    &// element "{DAE_NAMESPACE}texture"
     &| parseTexture
 
 parseTexture :: Cursor -> Texture
 parseTexture c =
   Texture
-    { textureSampler = attribute "texcoord" c
-    , textureTexcoord = attribute "texcoord" c
+    { textureSampler = fromMaybe "" $ listToMaybe $ attribute "sampler" c
+    , textureTexcoord = fromMaybe "" $ listToMaybe $ attribute "texcoord" c
     }
