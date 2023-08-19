8bbf8738da63c09894337b30d938f63b29fa7057
Steven <steven.a.pinkerton@gmail.com.com>
2023-08-19 03:23:17 +0100

    more fixes

--- src/AssetLoader/Effects.hs
+++ src/AssetLoader/Effects.hs
@@ -14,19 +14,21 @@ import AssetLoader.Datatypes
       Vector2Param(Vector2Param),
       FloatParam(FloatParam),
       EffectParam(..) )
-import Text.XML.Cursor ( attribute, element, ($//), (&|), Cursor )
+import Text.XML.Cursor ( attribute, element, ($//), (&|), Cursor, (&//) )
   
 parseEffects :: Cursor -> [Effect]
 parseEffects c =
   c
     $// element "{DAE_NAMESPACE}library_effects"
-    $// element "{DAE_NAMESPACE}effect"
+    &// element "{DAE_NAMESPACE}effect"
     &| parseEffect
 
 parseEffect :: Cursor -> Effect
 parseEffect c =
   Effect
-    { effectId = attribute "id" c
+    { effectId = case attribute "id" c of
+        [idValue] -> idValue
+        _ -> error "Unexpected number of id attributes"
     , effectParams = parseEffectParams c
     }
 
@@ -37,24 +39,29 @@ parseEffectParams c =
 parseEffectParam :: Cursor -> EffectParam
 parseEffectParam c =
   EffectParam
-    { paramSid = attribute "sid" c
+    { paramSid = case attribute "sid" c of
+        [sidValue] -> sidValue
+        _ -> error "Unexpected number of sid attributes"
     , paramType = parseEffectParamType c
     }
 
 parseEffectParamType :: Cursor -> ParamType
 parseEffectParamType c =
-  case lookupParamType (attribute "type" c) of
-    Just paramType -> paramType
-    Nothing -> error "Unknown effect param type"
+  case attribute "type" c of
+    [typeValue] -> case lookupParamType (toString typeValue) of
+      Just paramType' -> paramType'
+      Nothing -> error "Unknown effect param type"
+    _ -> error "Unexpected number of type attributes"
+
 
 lookupParamType :: String -> Maybe ParamType
 lookupParamType "SURFACE" = Just (SurfaceParam defaultSurface)
 lookupParamType "SAMPLER_STATE" = Just (SamplerParam defaultSampler)
 lookupParamType "LAMBERT" = Just (LambertParam defaultLambert)
 lookupParamType "CONSTANT" = Just (FloatParam 0)
-lookupParamType "FLOAT" = Just (FloatParam 0)
-lookupParamType "FLOAT2" = Just (Vector2Param zeroVector2)
-lookupParamType "FLOAT3" = Just (Vector3Param zeroVector3)
+lookupParamType "FLOAT" = FloatParam (FloatParam 0)
+lookupParamType "FLOAT2" = Vector2Param (Vector2Param zeroVector2)
+lookupParamType "FLOAT3" = Vector3Param (Vector3Param zeroVector3)
 lookupParamType "FLOAT4" = Just (Vector4Param zeroVector4)
 lookupParamType "FLOAT4x4" = Just (Matrix4x4Param identMatrix4x4)
 lookupParamType "BOOL" = Just (BoolParam False)
--- src/AssetLoader/VisualScene.hs
+++ src/AssetLoader/VisualScene.hs
@@ -1,22 +1,23 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use 'listToMaybe' from Relude" #-}
 module AssetLoader.VisualScene where
-import Text.XML.Cursor ( attribute, element, ($//), (&|), Cursor )
-import AssetLoader.Datatypes ( VisualScene(..), Node(..) )
-
-
+import Text.XML.Cursor ( attribute, element, ($//), (&|), Cursor, (&//), content, (&/), ($/) )
+import AssetLoader.Datatypes ( VisualScene(..), Node(..), Property (Property, propSid, propType, propValue) )
+import Data.Maybe ( fromJust )
 
 
 parseVisualScenes :: Cursor -> [VisualScene]
 parseVisualScenes cursor =
-  cursor
-    $// element "{DAE_NAMESPACE}library_visual_scenes"
-    $// element "{DAE_NAMESPACE}visual_scene"
-    &| parseVisualScene
+  let daeNs = "{DAE_NAMESPACE}"
+   in concatMap
+        (\c -> c $// element (daeNs <> "visual_scene") &| parseVisualScene)
+        (cursor $// element (daeNs <> "library_visual_scenes"))
 
 parseVisualScene :: Cursor -> VisualScene
 parseVisualScene c =
   VisualScene
-    { sceneId = attribute "id" c
-    , sceneName = attribute "name" c
+    { sceneId = fromJust $ listToMaybe $ attribute "id" c
+    , sceneName = fromJust $ listToMaybe $ attribute "name" c
     , sceneNodes = parseNodes c
     }
 
@@ -27,11 +28,33 @@ parseNodes c =
 parseNode :: Cursor -> Node
 parseNode c =
   Node
-    { nodeId = attribute "id" c
-    , nodeName = attribute "name" c
-    , nodeSid = attribute "sid" c
-    , nodeType = attribute "type" c
-    , nodeTransform = parseMatrix c
+    { nodeId = fromJust $ listToMaybe $ attribute "id" c
+    , nodeName = fromJust $ listToMaybe $ attribute "name" c
+    , nodeSid = fromJust $ listToMaybe $ attribute "sid" c
+    , nodeType = fromJust $ listToMaybe $ attribute "type" c
+    , nodeTransform = fromJust $ listToMaybe $ parseMatrix c
     , nodeChildren = parseNodes c
-    , nodeExtras = parseExtras c
+    , nodeExtras = fromJust $ listToMaybe $ parseExtras c
+    }
+
+newtype Extra = Extra
+  {extraValues :: [Property]}
+
+parseExtras :: Cursor -> [Extra]
+parseExtras c = do
+  extra <- c $// element "extra"
+  return $ Extra (parseProperties extra)
+
+parseProperties :: Cursor -> [Property]
+parseProperties c =
+  c $// element "technique"
+    &// element "value"
+    &| parseProperty
+
+parseProperty :: Cursor -> Property
+parseProperty c =
+  Property
+    { propSid = fromMaybe "" $ viaNonEmpty head (attribute "sid" c)
+    , propType = fromMaybe "" $ viaNonEmpty head (attribute "type" c)
+    , propValue = fromMaybe "" $ viaNonEmpty head (c $/ content)
     }
